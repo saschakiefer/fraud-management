0076304b9fed921e6a69955fd39553eb0668d1dc
Sascha Kiefer <mail@sascha-kiefer.de>
2022-03-25 12:15:15 +0100

    Chapter Commit

---
+++ customer/src/main/java/com/saschakiefer/customer/services/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.saschakiefer.customer.services;
+
+import com.saschakiefer.customer.CustomerRegistrationRequest;
+
+public interface ICustomerService {
+    public void registerCustomer(CustomerRegistrationRequest request);
+}
