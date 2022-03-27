f1101f3a7d9a9fce0e68d9148fee087b0b3a6352
Sascha Kiefer <mail@sascha-kiefer.de>
2022-03-27 14:07:22 +0200

    Add Service Discovery

--- customer/src/main/java/com/saschakiefer/customer/CustomerApplication.java
+++ customer/src/main/java/com/saschakiefer/customer/CustomerApplication.java
@@ -2,10 +2,11 @@ package com.saschakiefer.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class CustomerApplication
-{
+@EnableEurekaClient
+public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
     }
