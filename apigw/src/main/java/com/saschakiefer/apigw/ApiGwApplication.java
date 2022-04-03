af591e5485507a05234d1e3a4de0ba7dd9c8a3bf
Sascha Kiefer <mail@sascha-kiefer.de>
2022-04-03 15:54:50 +0200

    Add API Gateway

--- apigw/src/main/java/com/saschakiefer/apigw/ApiGwApplication.java
+++ apigw/src/main/java/com/saschakiefer/apigw/ApiGwApplication.java
@@ -1,13 +1,13 @@
-package com.saschakiefer.notification;
+package com.saschakiefer.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
-public class NotificationApplication {
+@SpringBootApplication
+public class ApiGwApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class, args);
+        SpringApplication.run(ApiGwApplication.class, args);
     }
 }
